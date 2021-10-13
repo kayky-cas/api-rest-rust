467b408d0246ef77b370bf412033629703eb8396
Kayky Casagrande <kayky-cas@github.com>
2021-10-12 21:55:58 -0300

    PUT and GET

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use parking_lot::RwLock;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use warp::{http, Filter};
+
+type Items = HashMap<String, i32>;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct Item {
+    name: String,
+    quantity: i32,
 }
+
+#[derive(Clone)]
+struct Store {
+    grocery_list: Arc<RwLock<Items>>,
+}
+
+impl Store {
+    fn new() -> Self {
+        Store {
+            grocery_list: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+}
+
+async fn add_album(item: Item, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    store.grocery_list.write().insert(item.name, item.quantity);
+
+    Ok(warp::reply::with_status(
+        "Added item to the list!",
+        http::StatusCode::CREATED,
+    ))
+}
+
+fn json_body() -> impl Filter<Extract = (Item,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+fn main() {}
